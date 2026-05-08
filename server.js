7e23474e5870ff229959de030aab833ff4a6169f
Stephanie Paul <lyvvout@gmail.com>
2026-05-07 23:15:42 -0400

    fix: use Twilio ambient hold music to fix queue error

--- server.js
+++ server.js
@@ -665,10 +665,10 @@ app.post("/twilio/incoming-live-call", async (req, res) => {
   console.log("QUEUING CALL INTO FLEX:", { from, callSid });
 
   // Enqueue the live call into TaskRouter/Flex
-  const enqueue = response.enqueue({
+ const enqueue = response.enqueue({
     workflowSid: process.env.TWILIO_WORKFLOW_SID,
-    waitUrl: `${process.env.BASE_URL}/twilio/hold-music`,
-    waitUrlMethod: "POST",
+    waitUrl: 'https://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient',
+    waitUrlMethod: "GET",
     action: `${process.env.BASE_URL}/twilio/queue-fallback`,
     method: "POST",
   });
